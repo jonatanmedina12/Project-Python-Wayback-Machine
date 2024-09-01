9aba0ae64b48425bb4887006fcd1966a60f3b3cb
Jonatan Medina <136943363+jonatanmedina12@users.noreply.github.com>
2024-09-01 15:39:21 -0500

    readmed
    readmed

--- src/controller/historicalController.py
+++ src/controller/historicalController.py
@@ -4,45 +4,50 @@ from waybackpy import WaybackMachineCDXServerAPI
 import  requests
 
 class HistorialSearch:
-    def __init__(self,url,user_agent):
-        self.url =url
+    def __init__(self, url, user_agent):
+        self.url = url
         self.user_agent = user_agent
-    def search_snapshot(self,year_ago=2,filename="snapshot.html"):
-        """guardar data"""
-        target_date = datetime.now() - timedelta(days=365*year_ago)
-        year , month_data,day = target_date.year,target_date.month,target_date.day
-        cdx_Api = WaybackMachineCDXServerAPI(self.url,self.user_agent)
-        snapshot=cdx_Api.near(year=year,month=month_data,day=day)
+
+    def search_snapshot(self, year_ago=2, filename="snapshot.html"):
+        """Busca y guarda una captura de pantalla de hace 'year_ago' años."""
+        target_date = datetime.now() - timedelta(days=365 * year_ago)
+        year, month_data, day = target_date.year, target_date.month, target_date.day
+        cdx_api = WaybackMachineCDXServerAPI(self.url, self.user_agent)
+        snapshot = cdx_api.near(year=year, month=month_data, day=day)
         if snapshot:
-            print(f"Fecha:{snapshot.timestamp}, URL :{snapshot.archive_url}")
-            self.download_snapshot(snapshot.archive_url,filename)
+            print(f"Fecha: {snapshot.timestamp}, URL: {snapshot.archive_url}")
+            self.download_snapshot(snapshot.archive_url, filename)
         else:
-            print("No se encontro ninguna captura")
+            print("No se encontró ninguna captura")
+
     @staticmethod
     def download_snapshot(archive_url, filename):
+        """Descarga y guarda una captura de pantalla."""
         response = requests.get(archive_url)
         if response.status_code == 200:
-            with open(filename,'w',encoding='utf-8') as file:
+            with open(filename, 'w', encoding='utf-8') as file:
                 file.write(response.text)
-            print(f"Documento guardado exitosamente en file {filename}")
+            print(f"Documento guardado exitosamente en el archivo {filename}")
         else:
-            print("Error al descargar el archivo")
+            print(f"Error al descargar el archivo: {response.status_code}")
 
-    def search_snapshots_by_extensions(self,year_ago=2, days_interval=30, extensiones=None, match_type="domain"):
+    def search_snapshots_by_extensions(self, year_ago=2, days_interval=30, extensiones=None, match_type="domain"):
+        """Busca capturas de pantalla por extensiones de archivo."""
         if extensiones is None:
-            extensiones =["pdf","doc","docx","ppt","xls","xlsx","txt"]
+            extensiones = ["pdf", "doc", "docx", "ppt", "xls", "xlsx", "txt"]
         today = datetime.now()
-        start_period = (today-timedelta(days=365 *year_ago)).strftime('%Y%m%d')
-        end_period = (today-timedelta(days=(365*year_ago) - days_interval)).strftime('%Y%m%d')
-        cdx_api = WaybackMachineCDXServerAPI(url=self.url,user_agent=self.user_agent,start_timestamp=start_period,end_timestamp=end_period,match_type=match_type)
+        start_period = (today - timedelta(days=365 * year_ago)).strftime('%Y%m%d')
+        end_period = (today - timedelta(days=(365 * year_ago) - days_interval)).strftime('%Y%m%d')
+        cdx_api = WaybackMachineCDXServerAPI(
+            url=self.url,
+            user_agent=self.user_agent,
+            start_timestamp=start_period,
+            end_timestamp=end_period,
+            match_type=match_type
+        )
 
-        # expresión regular (.*\\.pdf$||.\\*)
-
-        regex_filter = "("+ "|".join([f".*\\.{ext}$"for ext in extensiones]) + ")"
+        regex_filter = "(" + "|".join([f".*\\.{ext}$" for ext in extensiones]) + ")"
         cdx_api.filters = [f"urlkey:{regex_filter}"]
-        snapshot = cdx_api.snapshots()
-        for snapshots in snapshot:
-            print(f"Fecha:{snapshots.timestamp}, URL :{snapshots.archive_url}")
-
-
-
+        snapshots = cdx_api.snapshots()
+        for snapshot in snapshots:
+            print(f"Fecha: {snapshot.timestamp}, URL: {snapshot.archive_url}")
